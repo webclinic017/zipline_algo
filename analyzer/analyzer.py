b14da4b3b502b86e08410701c82159037734c2f7
DAI\nbhayani <nishant.bhayani@williamoneilindia.com>
2019-06-08 16:17:29 +0530

    data for positions done

--- analyzer/analyzer.py
+++ analyzer/analyzer.py
@@ -1,19 +1,49 @@
-from PyQt5 import QtWidgets
-from analyzer.views.main import AnalyzerWindow
-import sys
-import pandas as pd
-from analyzer.analysis_data import AnalysisData
 import datetime
-from pandas.tseries.offsets import BDay
-import numpy as np
+import os
+import sys
+from pathlib import Path
+
 import empyrical
+import numpy as np
+import pandas as pd
+from PyQt5 import QtWidgets
+from pandas.tseries.offsets import BDay
+
+from analyzer.analysis_data import AnalysisData
+from analyzer.views.main import AnalyzerWindow
 
 
 class Analyzer:
     def __init__(self, strategy):
+        self.sector_file = 'NASDAQ_sids.npy'
+        self.sector_data = np.load(os.path.join(str(Path.home()), self.sector_file))
+        self.sector_code_mapping = {0: "Basic Industries",
+                                    1: "Capital Goods",
+                                    2: "Consumer Durables",
+                                    3: "Consumer Non-Durables",
+                                    4: "Consumer Services",
+                                    5: "Energy",
+                                    6: "Finance",
+                                    7: "Health Care",
+                                    8: "Miscellaneous",
+                                    9: "Public Utilities",
+                                    10: "Technology",
+                                    11: "Transportation"}
         self.app = QtWidgets.QApplication(sys.argv)
         self.daily_data_df = pd.DataFrame(columns=['date', 'net'])
         self.daily_data_df.set_index('date', inplace=True)
+        self.daily_positions_df = pd.DataFrame(columns=['date',
+                                                        'symbol',
+                                                        'name',
+                                                        'sector',
+                                                        'avg_price',
+                                                        'last_price',
+                                                        'daily_change',
+                                                        'pct_daily_change',
+                                                        'total_change',
+                                                        'pct_total_change',
+                                                        'pct_port'])
+        self.daily_positions_df.set_index(['date', 'symbol'], inplace=True)
 
         self.analysis_data = AnalysisData()
         self.strategy = strategy
@@ -30,7 +60,49 @@ class Analyzer:
         pass
 
     def handle_data(self, context):
+        previous_date = context.datetime.date() if self.daily_data_df.empty else self.daily_data_df.index[-1]
+        previous_days_position = \
+            self.daily_positions_df.loc[self.daily_positions_df.index.get_level_values('date') == previous_date]
         self.daily_data_df.loc[context.datetime.date()] = [context.account.equity_with_loan]
+        columns = ['name',
+                   'sector',
+                   'avg_price',
+                   'last_price',
+                   'daily_change',
+                   'pct_daily_change',
+                   'total_change',
+                   'pct_total_change']
+
+        for position in context.portfolio.positions.values():
+            if (previous_date, position.asset.symbol) in previous_days_position.index:
+                previous_last_price = self.daily_positions_df.loc[previous_date, position.asset.symbol].last_price
+                daily_change = position.last_sale_price - previous_last_price
+                pct_daily_change = position.last_sale_price / previous_last_price - 1
+            else:
+                daily_change = 0
+                pct_daily_change = 0
+
+            total_change = position.last_sale_price - position.cost_basis
+            pct_total_change = position.last_sale_price / position.cost_basis - 1
+            self.daily_positions_df.loc[(context.datetime.date(),
+                                    position.asset.symbol), columns] = [position.asset.asset_name,
+                                                               self.sector_code_mapping.get(
+                                                                   self.sector_data[
+                                                                       position.asset.sid],
+                                                                   'NA'),
+                                                               position.cost_basis,
+                                                               position.last_sale_price,
+                                                               daily_change,
+                                                               pct_daily_change,
+                                                               total_change,
+                                                               pct_total_change
+                                                               ]
+
+        # self.daily_positions_df.pct_daily_change = \
+        #     self.daily_positions_df.groupby(level='symbol')['last_price'].pct_change()
+        #
+        # self.daily_positions_df.daily_change = \
+        #     self.daily_positions_df.groupby(level='symbol')['last_price'].diff() * -1
 
         if self.daily_data_df.shape[0] % 21 == 0:
             self.generate_analysis_data(context)
@@ -40,18 +112,19 @@ class Analyzer:
     def generate_analysis_data(self, context):
 
         if self.daily_data_df.shape[0] > 0:
-
             # Calculate returns
             daily_returns = self.daily_data_df['net'].pct_change()
 
             # daily return for the first day will always be 0
             daily_returns[0] = (self.daily_data_df['net'][0] / self.analysis_data.info_data['initial_cash']) - 1
 
-            ytd_returns = daily_returns[daily_returns.index >= datetime.datetime(daily_returns.index[-1].year, 1, 1).date()]
+            ytd_returns = daily_returns[
+                daily_returns.index >= datetime.datetime(daily_returns.index[-1].year, 1, 1).date()]
 
             one_year_daily_returns = daily_returns[daily_returns.index >= (daily_returns.index[-1] - BDay(252)).date()]
 
-            benchmark_returns = context.trading_environment.benchmark_returns.loc[self.daily_data_df.index[0]:self.daily_data_df.index[-1]]
+            benchmark_returns = context.trading_environment.benchmark_returns.loc[
+                                self.daily_data_df.index[0]:self.daily_data_df.index[-1]]
             benchmark_returns.index = benchmark_returns.index.date
 
             daily_returns = daily_returns.drop(daily_returns.index.difference(benchmark_returns.index))
@@ -60,7 +133,8 @@ class Analyzer:
             ytd_benchmark_returns = benchmark_returns[
                 benchmark_returns.index >= datetime.datetime(benchmark_returns.index[-1].year, 1, 1).date()]
 
-            one_year_benchmark_returns = benchmark_returns[benchmark_returns.index >= (benchmark_returns.index[-1] - BDay(252)).date()]
+            one_year_benchmark_returns = benchmark_returns[
+                benchmark_returns.index >= (benchmark_returns.index[-1] - BDay(252)).date()]
 
             portfolio_dd = self.rolling_drawdown(daily_returns.values)
             benchmark_dd = self.rolling_drawdown(benchmark_returns.values)
@@ -101,30 +175,31 @@ class Analyzer:
             plot_data_df.set_index('date', inplace=True)
 
             # holdings data
-            holdings = pd.DataFrame(columns=['symbol',
-                                             'name',
-                                             'sector',
-                                             'avg_price',
-                                             'last_price',
-                                             'daily_change',
-                                             'pct_daily_change',
-                                             'total_change',
-                                             'pct_total_change',
-                                             'pct_port'])
-
-            holdings.at[0] = ['symbol',
-                               'name',
-                               'sector',
-                               4, 5, 6, 7, 8, 9, 10]
-            holdings.at[1] = ['symbol1',
-                              'name1',
-                              'sector1',
-                              41, 51, 61, 71, 81, 91, 101]
+            # holdings = pd.DataFrame(columns=['symbol',
+            #                                  'name',
+            #                                  'sector',
+            #                                  'avg_price',
+            #                                  'last_price',
+            #                                  'daily_change',
+            #                                  'pct_daily_change',
+            #                                  'total_change',
+            #                                  'pct_total_change',
+            #                                  'pct_port'])
+            #
+            # holdings.at[0] = ['symbol',
+            #                   'name',
+            #                   'sector',
+            #                   4, 5, 6, 7, 8, 9, 10]
+            # holdings.at[1] = ['symbol1',
+            #                   'name1',
+            #                   'sector1',
+            #                   41, 51, 61, 71, 81, 91, 101]
 
             self.analysis_data.chart_data = plot_data_df
             self.analysis_data.strategy_report = report_dict
             self.analysis_data.benchmark_report = benchmark_report_dict
-            self.analysis_data.holdings_data = holdings
+            self.analysis_data.holdings_data = \
+                self.daily_positions_df.loc[self.daily_positions_df.index.get_level_values('date') == context.datetime.date()]
 
     def rolling_drawdown(self, returns):
         out = np.empty(returns.shape[1:])
@@ -142,7 +217,7 @@ class Analyzer:
         returns_array = np.asanyarray(returns)
 
         cumulative = np.empty(
-            (returns.shape[0] + 1, ) + returns.shape[1:],
+            (returns.shape[0] + 1,) + returns.shape[1:],
             dtype='float64'
         )
 
---
+++ buy_hold_apple/algo.py
@@ -0,0 +1,47 @@
+import pandas as pd
+from zipline.api import order_target, symbol
+from strategy import Strategy
+
+def initialize(context):
+    pass
+
+def handle_data(context, data):
+    stock = symbol('AAPL')
+    stock1 = symbol('GOOG')
+    order_target(stock, 100)
+    order_target(stock1, 100)
+
+def analyze(context, data):
+    pass
+
+def before_trading_start(context, data):
+    pass
+
+if __name__ == '__main__':
+    # start date for the backtest in yyyymmdd format string
+    start_date = '20150101'
+    # converting date string to date
+    start_date = pd.to_datetime(start_date, format='%Y%m%d').tz_localize('UTC')
+
+    # end date for the backtest in yyyymmdd format string
+    end_date = '20190331'
+    end_date = pd.to_datetime(end_date, format='%Y%m%d').tz_localize('UTC')
+
+    # The run_algorithm is a function provided by zipline that initializes and calls all the functions like before_
+    # trading_start, handle_data etc etc in the prescribed order, thereby running our backtest from the defined
+    # start till the end date, doing all the buy and sells with the starting capital defined as capital_base and using
+    # the data bundle defined as bundle (in our case quandl
+
+    kwargs = {'start': start_date,
+              'end': end_date,
+              'initialize': initialize,
+              'handle_data': handle_data,
+              'analyze': analyze,
+              'before_trading_start': before_trading_start,
+              'bundle': 'quandl',
+              'capital_base': 100000,
+              'algo_name': 'mid_term_low_risk',
+              'benchmark_symbol': 'SPY'}
+
+    strategy = Strategy(kwargs)
+    strategy.run_algorithm()
