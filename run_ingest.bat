bd4cea3ede3588cc54883a7795a6a19282f09fc5
Quantelioalgo <quantelioalgo@gmail.com>
2019-06-15 18:30:24 +0530

    updated ingest file

--- run_ingest.bat
+++ run_ingest.bat
@@ -10,3 +10,9 @@ set path=%anaconda%\Library\bin;%path%
 zipline ingest -b quandl
 
 CALL %anaconda%\Scripts\deactivate.bat
+
+python alpha-compiler-master\alphacompiler\data\load_quandl_sf1.py
+
+python alpha-compiler-master\alphacompiler\data\NASDAQ.py
+
+python alpha-compiler-master\alphacompiler\data\NASDAQ_sector_code_loader.py
