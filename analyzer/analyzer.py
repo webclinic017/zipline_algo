0e314bdb53824b6edb0122fcfc52df6a9b7bac67
Quantelioalgo <quantelioalgo@gmail.com>
2019-06-29 08:31:12 +0000

    30 days transactions not visible bug fixed

--- analyzer/analyzer.py
+++ analyzer/analyzer.py
@@ -232,7 +232,7 @@ class Analyzer:
             if len(self.daily_positions_df.index.get_level_values('date').value_counts()) < 30:
                 self.analysis_data.holdings_data = self.daily_positions_df.reset_index()
             else:
-                self.analysis_data.holdings_data = self.daily_positions_df.loc[self.daily_positions_df.index.get_level_values('date') >= self.daily_positions_df.index.get_level_values('date')[-30]].reset_index()
+                self.analysis_data.holdings_data = self.daily_positions_df.loc[self.daily_positions_df.index.get_level_values('date') >= self.daily_positions_df.index.get_level_values('date').value_counts().sort_index().index[-30]].reset_index()
             if len(self.daily_data_df.index) < 30:
                 self.analysis_data.transactions_data = self.transactions_data
             else:
