7da513f5d7f146a0d74dc5d1bf4564d35255ce21
Quantelioalgo <quantelioalgo@gmail.com>
2019-07-20 14:48:28 +0000

    live mode

--- strategy.py
+++ strategy.py
@@ -27,14 +27,27 @@ class Strategy:
         self.analyzer.before_trading_start()
 
     def run_algorithm(self):
-        kwargs = {'start': self.strategy_data.get('start'),
-                  'end': self.strategy_data.get('end'),
-                  'initialize': self.initialize,
-                  'handle_data': self.handle_data,
-                  'analyze': self.analyze,
-                  'before_trading_start': self.before_trading_start,
-                  'bundle': 'quandl',
-                  'capital_base': self.strategy_data.get('capital_base')}
+        if self.strategy_data.get('live_trading', False) is False:
+            kwargs = {'start': self.strategy_data.get('start'),
+                      'end': self.strategy_data.get('end'),
+                      'initialize': self.initialize,
+                      'handle_data': self.handle_data,
+                      'analyze': self.analyze,
+                      'before_trading_start': self.before_trading_start,
+                      'bundle': 'quandl',
+                      'capital_base': self.strategy_data.get('capital_base')}
+        else:
+            kwargs = {'start': self.strategy_data.get('start'),
+                      'end': self.strategy_data.get('end'),
+                      'initialize': self.initialize,
+                      'handle_data': self.handle_data,
+                      'analyze': self.analyze,
+                      'before_trading_start': self.before_trading_start,
+                      'bundle': 'quandl',
+                      'capital_base': self.strategy_data.get('capital_base'),
+                      'tws_uri': 'localhost:7497:1232',
+                      'live_trading': True}
+
         run_algo_thread = threading.Thread(target=run_algorithm, kwargs=kwargs)
         run_algo_thread.start()
 
