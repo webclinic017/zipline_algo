9f7ee46115eabb81e15a39023afef00c15827626
DAI\nbhayani <nishant.bhayani@williamoneilindia.com>
2019-09-18 14:27:11 +0530

    get sector and ipl from quandl instead of nasdaq

--- alpha-compiler-master/alphacompiler/data/NASDAQ_sector_code_loader.py
+++ alpha-compiler-master/alphacompiler/data/NASDAQ_sector_code_loader.py
@@ -12,6 +12,8 @@ from alphacompiler.util.zipline_data_tools import get_ticker_sid_dict_from_bundl
 import os
 import requests
 from pathlib import Path
+from zipfile import ZipFile
+from six.moves.urllib.parse import urlencode
 
 # this gets all the data for the three exchanges 6000+ tickers
 BASE_URL = "http://www.nasdaq.com/screening/companies-by-industry.aspx?&render=download"
@@ -20,31 +22,61 @@ BASE_PATH = str(Path.home())
 RAW_FILE = "NASDAQ_table.csv"
 SID_FILE = "NASDAQ_sids.npy"  # persisted np.array where
 SID_FILE_IPO = "NASDAQ_sids_IPO.npy"
+from io import BytesIO
 
 # NASDAQ sectors, not the same as Morningstar
-SECTOR_CODING = {"Basic Industries": 0,
-           "Capital Goods": 1,
-           "Consumer Durables": 2,
-           "Consumer Non-Durables": 3,
-           "Consumer Services": 4,
+SECTOR_CODING = {"Industrials": 0,
+           "Basic Materials": 1,
+           "Consumer Cyclical": 2,
+           "Consumer Defensive": 3,
+           "Real Estate": 4,
            "Energy": 5,
-           "Finance": 6,
-           "Health Care": 7,
-           "Miscellaneous": 8,
-           "Public Utilities": 9,
+           "Financial Services": 6,
+           "Healthcare": 7,
+           "Communication Services": 8,
+           "Utilities": 9,
            "Technology": 10,
-           "Transportation": 11,
-           "n/a": -1}
+           "": -1}
 
-def create_sid_table_from_file(filepath):
+QUANDL_DATA_TICKERS_URL = (
+    # 'https://www.quandl.com/api/v3/datatables/WIKI/PRICES.csv?'
+    'https://www.quandl.com/api/v3/datatables/SHARADAR/TICKERS.csv?'
+)
+
+def format_metadata_tickers_url(api_key):
+    query_params = [('api_key', api_key), ('qopts.export', 'true')]
+
+    return (
+            QUANDL_DATA_TICKERS_URL + urlencode(query_params)
+    )
+
+def download_without_progress(url):
+    """
+    Download data from a URL, returning a BytesIO containing the loaded data.
+
+    Parameters
+    ----------
+    url : str
+        A URL that can be understood by ``requests.get``.
+
+    Returns
+    -------
+    data : BytesIO
+        A BytesIO containing the downloaded data.
+    """
+    resp = requests.get(url)
+    resp.raise_for_status()
+    return BytesIO(resp.content)
+
+def create_sid_table_from_file(ticker_df):
     """reads the raw file, maps tickers -> SIDS,
     then maps sector strings to integers, and saves
     to the file: SID_FILE"""
-    df = pd.read_csv(filepath, index_col="Symbol")
+    df = ticker_df
     df = df.drop_duplicates()
 
-    coded_sectors_for_ticker = df["Sector"].map(SECTOR_CODING)
-
+    coded_sectors_for_ticker = df["sector"].map(SECTOR_CODING)
+    coded_sectors_for_ticker.fillna(-1, inplace=True)
     ae_d = get_ticker_sid_dict_from_bundle('quandl')
     N = max(ae_d.values()) + 1
 
@@ -61,11 +93,11 @@ def create_sid_table_from_file(filepath):
     np.save(os.path.join(BASE_PATH , SID_FILE), sectors)
 
 
-def create_sid_table_from_file_ipo(filepath):
+def create_sid_table_from_file_ipo(ticker_df):
     """reads the raw file, maps tickers -> SIDS,
     then maps sector strings to integers, and saves
     to the file: SID_FILE"""
-    df = pd.read_csv(filepath, index_col="Symbol")
+    df = ticker_df
     df = df.drop_duplicates()
 
     # coded_sectors_for_ticker = df["Sector"].map(SECTOR_CODING)
@@ -79,7 +111,7 @@ def create_sid_table_from_file_ipo(filepath):
     # iterate over Assets in the bundle, and fill in sectors
     for ticker, sid in ae_d.items():
         try:
-            ipoyears[sid] = df.loc[ticker].IPOyear
+            ipoyears[sid] = int(df.loc[ticker].firstpricedate[:4])
         except Exception as e:
             print(e)
 
@@ -88,8 +120,31 @@ def create_sid_table_from_file_ipo(filepath):
 
 if __name__ == '__main__':
     INPUT_FILE = os.path.join(BASE_PATH , RAW_FILE)
-    r = requests.get(BASE_URL, allow_redirects=True)
-    open(INPUT_FILE, 'wb').write(r.content)
-    create_sid_table_from_file(INPUT_FILE)
-    create_sid_table_from_file_ipo(INPUT_FILE)
+    # r = requests.get(BASE_URL, allow_redirects=True)
+    # open(INPUT_FILE, 'wb').write(r.content)
+    metadata_tickers = pd.read_csv(
+        format_metadata_tickers_url('xk5E9bJuWAw5zYjhRH_S')
+    )
+    table_tickers_url = metadata_tickers.loc[0, 'file.link']
+    raw_file_tickers = download_without_progress(table_tickers_url)
+
+    with ZipFile(raw_file_tickers) as zip_file:
+        file_names = zip_file.namelist()
+        assert len(file_names) == 1, "Expected a single file from Quandl."
+        wiki_prices = file_names.pop()
+        with zip_file.open(wiki_prices) as table_file:
+            with zip_file.open(wiki_prices) as table_file:
+                data_table_tickers = pd.read_csv(
+                    table_file,
+                    index_col='ticker',
+                    usecols=[
+                        'ticker',
+                        # 'action',
+                        'sector',
+                        'firstpricedate',
+                    ],
+                )
+
+    create_sid_table_from_file(data_table_tickers)
+    create_sid_table_from_file_ipo(data_table_tickers)
     print("all done boss")
