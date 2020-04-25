5c6f379762b0a5d1127a9238a94a0e98d42260c8
Quantelioalgo <quantelioalgo@gmail.com>
2020-04-25 15:45:20 -0400

    add new algos (alpha, beta and ltlr with daily SL)

---
+++ alpha/alpha_config.py
@@ -0,0 +1,10 @@
+config = {
+    # start and end date for the backtest in yyyymmdd format string
+    'start_date': 20090101,
+    'end_date': 20190101,
+    # capital to start with
+    'capital_base': 100000,
+    'benchmark_symbol': 'SPY',
+    'name': 'alpha',
+    'id': 1
+}
