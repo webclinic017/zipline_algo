1a3ddc435604fcb717f36a79ba73bf3cfcd94ea0
Chicago-Joe <jloss94@gmail.com>
2020-05-01 04:51:10 +0000

    uploaded fnTradingCritera.py for my trading criteria functions and also created a copy algorithm named: ltlr_algo_JL.py for my work and testing purposes

--- long_term_low_risk/ltlr_config.py
+++ long_term_low_risk/ltlr_config.py
@@ -1,9 +1,8 @@
 config = {
     # start and end date for the backtest in yyyymmdd format string
-    'start_date': 19990901,
-    'end_date': 20191001,
-    # capital to start with
-    'capital_base': 100000,
+    'start_date': 19990102,
+    'end_date': 20200102,
+    'capital_base': 1000000,
     'benchmark_symbol': 'SPY',
     'name': 'long_term_low_risk',
     'id': 1
