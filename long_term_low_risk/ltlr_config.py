0ef198a4c26b7d27d1e9db7e93d21d387b6e44e9
Chicago-Joe <jloss94@gmail.com>
2020-05-12 17:56:08 -0400

    changed ltlr_config start date to 2006-08-01 (transactions data SF2 starts in mid-2005)

--- long_term_low_risk/ltlr_config.py
+++ long_term_low_risk/ltlr_config.py
@@ -1,7 +1,7 @@
 config = {
     # start and end date for the backtest in yyyymmdd format string
-    'start_date': 19990901,
-    'end_date': 20200102,
+    'start_date': 20060801,
+    'end_date': 20200501,
     'capital_base': 1000000,
     'benchmark_symbol': 'SPY',
     'name': 'long_term_low_risk',
