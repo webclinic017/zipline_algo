534fa0c63e9130d8ee967e5154c446aaa2bc04be
Quantelioalgo <quantelioalgo@gmail.com>
2020-06-02 14:25:42 -0400

    api controller related changes (missing file)

---
+++ utils/order_controller.py
@@ -0,0 +1,15 @@
+from zipline.api import order_target_percent, order_target
+
+
+def order_target_percent():
+    return order_target_percent()
+
+
+def order_target():
+    return order_target()
+
+
+def order():
+    return order()
+
+
